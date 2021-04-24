9fd09cbd2ff67e7e64b21e3da06d159e4b948900
fmabid <fmaabid@gmail.com>
2021-04-24 14:07:21 +0600

    init

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div>
+      <Carousel :per-page="1" :navigate-to="someLocalProperty" :mouse-drag="false">
+        <Slide>
+          Slide 1 Content
+        </Slide>
+        <Slide>
+          Slide 2 Content
+        </Slide>
+      </Carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+// import VueCarousel from '@chenfengyuan/vue-carousel';
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+  data() {
+    return {
+      data: [
+        '<div class="example-slide">Slide 1</div>',
+        '<div class="example-slide">Slide 2</div>',
+        '<div class="example-slide">Slide 3</div>',
+      ],
+    };
+  },
+}
+</script>
+
