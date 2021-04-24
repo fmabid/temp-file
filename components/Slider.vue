4ff69a1a63eceba50e323db0cb84a6b55c6c711d
fmabid <fmaabid@gmail.com>
2021-04-24 20:44:13 +0600

    sliders

---
+++ components/Slider.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Carousel
+      :per-page="1"
+      :navigate-to="someLocalProperty"
+      :mouse-drag="false"
+    >
+      <Slide>
+        <img src="https://devxhub.com/storage/uploads/sliders/1619072540-x-services-content.jpg" alt="">
+      </Slide>
+      <Slide>
+        <img src="https://devxhub.com/storage/uploads/sliders/1619072540-x-services-content.jpg" alt="">
+      </Slide>
+      <Slide>
+        <img src="https://devxhub.com/storage/uploads/sliders/1619072540-x-services-content.jpg" alt="">
+      </Slide>
+    </Carousel>
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ components/Slider2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-5/6">
+    <slide :data="slide" />
+  </div>
+</template>
+
+<script>
+import slide from "@wyhaya/vue-slide";
+
+export default {
+  components: { slide },
+  data() {
+    return {
+      slide: [
+        "https://devxhub.com/storage/uploads/sliders/1619072540-x-services-content.jpg",
+        "https://devxhub.com/storage/uploads/sliders/1619072540-x-services-content.jpg",
+        "https://devxhub.com/storage/uploads/sliders/1619072540-x-services-content.jpg",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
